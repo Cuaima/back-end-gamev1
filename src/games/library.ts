86bf5e41d3426956e690db9cf78aee7b3b6cac9b
Claudia Groot <claudia@groot.me>
2018-05-30 15:15:56 +0200

    Update library.ts

--- src/games/library.ts
+++ src/games/library.ts
@@ -3,9 +3,7 @@ export const colorList = ["red", "blue", "green", "yellow", "magenta"]
 export const changeColor = () => colorList[Math.floor(Math.random() * colorList.length)]
 
 //Turns board into a string and then to a json file (so it can be stored in the database)
-const defaultBoard = [['o', 'o', 'o'],['o', 'o', 'o'],['o', 'o', 'o']]
-const stringifiedBoard = JSON.stringify(defaultBoard)
-export const jsonBoard = JSON.parse(stringifiedBoard)
+export const jsonBoard = [['o', 'o', 'o'],['o', 'o', 'o'],['o', 'o', 'o']]
 
 //Count the difference of moves from one board to another
 export const moves = (board1, board2) => 
